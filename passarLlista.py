b2ad886d98dd1506afe2a159c7bbd0fba444aa54
guillemJDA24 <guillem.perez@iesjoandaustria.org>
2020-05-12 11:37:40 +0000

    ex3

--- passarLlista.py
+++ passarLlista.py
@@ -38,22 +38,22 @@ while (opcioMenu !=5):
       alumne=i.split(',')
       ##TODO Pot una laumne tenir dos edats a la vegada?
       if (alumne[1] == '11'):
-        eso1.write(alumne[0] + alumne[2] + alumne[5]  )
+        eso1.write(alumne[0] +"," + alumne[2] + ','+alumne[5]  )
       elif (alumne[1] =='12') :
-        eso1.write(alumne[0] + alumne[2] + alumne[5]  )
+        eso1.write(alumne[0] +','+ alumne[2] + ','+alumne[5]  )
       elif (alumne[1] =='13'):
-        eso2.write(alumne[0] + alumne[2] + alumne[5]  )
+        eso2.write(alumne[0] + ','+alumne[2] +','+ alumne[5]  )
       elif (alumne[1] =='14'):
-        eso3.write(alumne[0] + alumne[2] + alumne[5]  )
+        eso3.write(alumne[0] + ','+alumne[2] + ','+alumne[5]  )
       elif (alumne[1] =='15'):
-        eso4.write(alumne[0] + alumne[2] + alumne[5]  )
+        eso4.write(alumne[0] +','+ alumne[2] + ','+alumne[5]  )
       elif (alumne[1] =='16'):
-        bat1.write(alumne[0] + alumne[2] + alumne[5]  )
+        bat1.write(alumne[0] + ','+alumne[2] +','+ alumne[5]  )
       ##TODO Pot una laumne tenir dos edats a la vegada?  
       elif (alumne[1] =='17'):
-        bat2.write(alumne[0] + alumne[2] + alumne[5]  )
+        bat2.write(alumne[0] +','+ alumne[2] + ','+alumne[5]  )
       elif (alumne[1]=='18'):
-        bat2.write(alumne[0] + alumne[2] + alumne[5]  )
+        bat2.write(alumne[0] +','+ alumne[2] + ','+alumne[5]  )
     eso1.close()
     eso2.close()
     eso3.close()
@@ -73,13 +73,13 @@ while (opcioMenu !=5):
     if (curs =='eso1'):
       eso1asis=open(directoriAssistencia+'/eso1_'+str(dia)+'.csv','w')
       for n in eso1:
-        alumne = eso1.readline().split()
+        alumne = n.split(',')
         print(alumne[0])
         asis=input('esta alumne 1=si 0=no')
         if asis == '1':
-          eso1asis.write(alumne[0]+'- 1')
+          eso1asis.write(' '+alumne[0]+'-1')
         elif (asis == '0'):
-          eso1asis.write(alumne[0]+'- 0')
+          eso1asis.write(' '+alumne[0]+'-0')
       eso1asis.close()   
         
 
